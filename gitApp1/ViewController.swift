a0baacc9db36ef4d92b6051695283432a59786c2
polyakovamarina <marina.yeah@mail.ru>
2022-11-05 15:25:55 +0900

    Initial Commit

---
+++ gitApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitApp1
+//
+//  Created by marinapolyakova on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
